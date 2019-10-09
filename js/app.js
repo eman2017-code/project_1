9af341947557a2c8abb4bccd6f64ffdc3c700032
Emmanuel Milloer <emmanuelmiller355@gmail.com>
2019-10-08 20:51:42 -0500

    added more words to master list of words

--- js/app.js
+++ js/app.js
@@ -12,31 +12,33 @@ const game = {
 		'coding', 'amazing', 'wonderful', 'computer', 'general', 'assembly', 'crazy',
 		'immaculate', 'bitter', 'iphone', 'time', 'coffee', 'javascript', 'ajax', 'protocol', 'water', 
 		'prestidigitation', 'charger', 'keyboard', 'shoe', 'shoelace', 
-		'belt', 'closet', 'inside', 'outside', 'exciting', 'wow', 'according', 'martin', 
+		'belt', 'closet', 'inside', 'outside', 'exciting', 'according', 'martin', 
 		'specialist', 'bioinformatics', 'biology', 'chemistry', 'popcorn', 'potatoe', 'worse', 'oblivious', 'young', 
 		'innocent', 'because', 'constitute', 'johnny', 'abject', 'aberration', 'abnegation', 'accost',  
-		'acceration', 'alias', 'smirk', 'divergents', 'hunger', 'games', 'key', 'piano', 'cup', 
-		'morning', 'tissue', 'flask', 'fire','cord', 'floor', 'wood', 'headphones', 'earjack', 
-		'heater', 'cooler', 'cologne', 'watch', 'apple', 'android', 'adidas', 'nike', 
-		'acceration', 'cologne', 'watch', 'apple', 'android', 'exam', 'test', 'spider', 'arachnid', 
-		'attack', 'warning', 'hat', 'snow', 'examine', 'sneaker', 'cover', 'candle', 'fire', 'acceration', 'alias', 'smirk',
-		'cologne', 'watch', 'apple', 'android', 'adidas', 'nike', 'backpack', 'lipstick', 'exam', 'test', 'spider', 
-		'arachnid', 'wow', 'attack', 'warning', 'hat', 'snow','examine', 'sneaker', 'cover', 'candle', 'fire', 
-		'adidas', 'nike',,'nike', 'backpack', 'lipstick', 'divergents', 'hunger', 'games', 
-		'key', 'piano', 'cup','ocean', 'camera', 'recording', 'precipitation', 
-		'interesting', 'execution', 'socks','underwear', 'foot', 'portrait', 'teeth', 'chin', 'book', 'books', 
-		'deodorant', 'brush', 'white board', 'chalk board', 'receive', 'port', 'deck', 'ship', 
-		'acknowledge', 'realm', 'underwear', 'foot', 'portrait', 'teeth', 'chin', 'book', 'books', 
-		'deorderant', 'brush', 'white board', 'chalk board', 'receive', 'port', 'deck', 'ship', 
-		'realm', 'pound', 'jump', 'caresses', 'suburban', 'city',
-		'liquid', 'solid', 'chicago', 'obstinate', 'hardest', 'softest', 'vocabulary', 'apathetic', 'arbitrary', 'arrogate'
+		'alias', 'smirk', 'divergents', 'hunger', 'games', 'piano', 'cup', 
+		'morning', 'tissue', 'flask', 'fire', 'cord', 'floor', 'wood', 'headphones', 'earjack', 
+		'heater', 'cooler', 'apple', 'android', 'acceration', 'exam', 'test', 'spider', 'attack', 'hat', 
+		'snow', 'examine', 'cover', 'candle','cologne', 'watch','backpack', 'lipstick', 'arachnid', 'wow', 'warning', 
+		'sneaker', 'adidas', 'nike', 'key','ocean', 'camera', 'recording', 'precipitation', 
+		'interesting', 'execution', 'socks', 'teeth', 'chin', 'book', 'books', 'deodorant', 'chalk board', 'receive', 
+		'port', 'deck', 'ship', 'acknowledge', 'underwear', 'foot', 'portrait', 'deorderant', 'brush', 'white board', 
+		'realm', 'pound', 'jump', 'caresses', 'suburban', 'city','liquid', 'solid', 'chicago', 
+		'obstinate', 'hardest', 'softest', 'vocabulary', 'apathetic', 'arbitrary', 'arrogate', 'crazy', 'zoo'
 	],
 
 	collectPlayerInfo() {
 		//collect the level selected and chosen gamertag of the user
 		const $gamerTag = $('#input-box').val().toUpperCase();
-		// console.log($gamerTag);
+
 		const $gamerDifficulty = $('#difficultyButton').val().toUpperCase();
+		if($gamerDifficulty == 'hard') {
+			// have the showWord function print a word every second
+		} else if ($gamerDifficulty == 'medium') {
+			// have the showWord function print a word every 2 seconds
+		} else {
+			// have the showWord function print a word every 3 seconds
+		}
+
 		const $h5 = $('h5');
 		const $h4 = $('h4');
 		$h5.html('Welcome: ' + $gamerTag);
@@ -45,25 +47,28 @@ const game = {
 		$('body').css('background-image', 'none')
 		$('body').css('background-color', '#324759');
 		$('.gameSection').show();
-		// this.endGame();
 	},
  
 	startGame() {
-		// // the timer will start to count from 0 up
-		// const interval = setInterval(() => {
-		// 	this.timer ++;
-		// 	$timer = $('.timer');
-		// 	$timer.html('Timer: ' + this.timer)
-		// 	console.log(this.timer);
-		// 	// set a duration for 1 second for the timer
-		// }, 1000);
+		// the timer will start to count from 0 up
+		const interval = setInterval(() => {
+			this.timer ++;
+			$timer = $('.timer');
+			$timer.html('Timer: ' + this.timer)
+			console.log(this.timer);
+			// set a duration for 1 second for the timer
+			if($('.wordDiv').length === 15) {
+				clearInterval(interval);
+			}
+		}, 1000);
 
 		this.unUsedWords = this.words.slice();
 		this.showInfo();
+		// this.showWord($gamerDifficulty);
 		this.showWord();
 	},
 
-	showWord() {
+	showWord(gamerSpeedChoice) {
 		setTimeout(() => {
 			// this is my base case
 			if(this.unUsedWords.length ===  0 || this.isGameOver === true) {
@@ -76,13 +81,11 @@ const game = {
 				const $div = $('<div class="wordDiv"></div>');
 				// put in the word that was randomly generated into the div
 				$div.text(this.unUsedWords[randomWordIndex]).css('display', 'none');
-				// console.log(this.words[randomWordIndex] + '<--- random word selected from original words array');
 				// put those divs into the word section so they appear for the user
 				$('.words').append($div);
 				$div.slideDown();
 				// remove it from the words array
 				const wordOnScreen = this.unUsedWords.splice(randomWordIndex, 1)[0];
-				// console.log($wordsOnScreen);
 				this.usedWords.push(wordOnScreen);
 				// then show the next word
 				this.showWord();
@@ -127,14 +130,13 @@ const game = {
 	endGame() {
 		// when one of the divs hit the bottom of the page the game is over
 
-		// console.log($wordDiv.css('height'))
 		if($('.wordDiv').length == 15) {
 			const $h1 = $('<h1></h1>');
 			const $div = $('.gameOver')
 			$h1.html('YOU ARE DEAD!!!!')
 			$div.append($h1);
 			// console.log('YOU DIED!!!');	
-			this.isGameOver	= true
+			this.isGameOver	= true;
 		} 
 	}
 }
